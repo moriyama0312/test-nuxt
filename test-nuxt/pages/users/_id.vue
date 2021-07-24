7856711b7b77036d640954df312d263a849d06b2
Moriyama Masataka <masataka.312@gmail.com>
2021-07-24 14:11:52 +0900

    init routing

---
+++ test-nuxt/pages/users/_id.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+		<p>ID is {{ userId }}</p>
+	</div>
+</template>
+<script lang="ts">
+export default {
+	data() {
+		return {
+			userId: this.$route.params.id
+		};
+	}
+};
+</script>
