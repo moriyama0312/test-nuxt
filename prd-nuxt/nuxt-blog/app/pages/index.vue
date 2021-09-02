21058320884fabb38f632fba27fe4465854c6144
Moriyama Masataka <masataka.312@gmail.com>
2021-09-03 08:50:26 +0900

    markup

--- prd-nuxt/nuxt-blog/app/pages/index.vue
+++ prd-nuxt/nuxt-blog/app/pages/index.vue
@@ -1,75 +1,45 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        nuxt-blog
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+	<section class="container">
+		<el-card style="flex: 1;">
+			<div slot="header" class="clearfix">
+				<span>ログイン</span>
+			</div>
+			<form>
+				<div class="form-content">
+					<span>ユーザーID</span>
+					<el-input placeholder="" v-model="formData.id" />
+				</div>
+				<div class="form-content">
+					<el-checkbox v-model="isCreateMode">アカウントを作成する</el-checkbox>
+				</div>
+				<div class="text-right">
+					<el-button type="primary">{{ buttonText }}</el-button>
+				</div>
+			</form>
+		</el-card>
+	</section>
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
-
-export default Vue.extend({})
+<script>
+export default {
+	asyncData()	{
+		return {
+			isCreatedMode: false,
+			formData: {
+				id: ''
+			}
+		};
+	},
+	computed: {
+		buttonText() {
+			return this.isCreatedMode ? '新規作成' : 'ログイン';
+		}
+	}
+};
 </script>
 
 <style>
-.container {
-	margin: 0 auto;
-	min-height: 100vh;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	text-align: center;
-}
-
-.title {
-	font-family:
-		'Quicksand',
-		'Source Sans Pro',
-		-apple-system,
-		BlinkMacSystemFont,
-		'Segoe UI',
-		Roboto,
-		'Helvetica Neue',
-		Arial,
-		sans-serif;
-	display: block;
-	font-weight: 300;
-	font-size: 100px;
-	color: #35495e;
-	letter-spacing: 1px;
-}
-
-.subtitle {
-	font-weight: 300;
-	font-size: 42px;
-	color: #526488;
-	word-spacing: 5px;
-	padding-bottom: 15px;
-}
-
-.links {
-	padding-top: 15px;
+.form-content {
+	margin: 16px 0;
 }
 </style>
